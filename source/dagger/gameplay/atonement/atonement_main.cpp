d0f7b1f74f85c27f09397563ad625d215030e359
Jovan Djordjevic <joca.dj@hotmail.com>
2021-05-03 19:44:23 +0200

    loading a test level

--- source/dagger/gameplay/atonement/atonement_main.cpp
+++ source/dagger/gameplay/atonement/atonement_main.cpp
@@ -1,6 +1,70 @@
 #include "atonement_main.h"
 
-atonement_main::atonement_main()
-{
+#include "core/core.h"
+#include "core/engine.h"
+#include "core/input/inputs.h"
+#include "core/graphics/sprite.h"
+#include "core/graphics/animation.h"
+#include "core/graphics/shaders.h"
+#include "core/graphics/window.h"
+#include "core/game/transforms.h"
+#include "core/graphics/sprite_render.h"
+#include "core/graphics/textures.h"
+#include "core/graphics/animations.h"
+#include "core/graphics/gui.h"
+#include "tools/diagnostics.h"
 
+#include "gameplay/common/simple_collisions.h"
+#include "gameplay/editor/savegame_system.h"
+#include "gameplay/editor/editor_main.h"
+
+
+using namespace dagger;
+using namespace atonement;
+using namespace editor;
+
+void AtonementGame::CoreSystemsSetup()
+{
+    auto& engine = Engine::Instance();
+
+    engine.AddSystem<WindowSystem>();
+    engine.AddSystem<InputSystem>();
+    engine.AddSystem<ShaderSystem>();
+    engine.AddSystem<TextureSystem>();
+    engine.AddSystem<SpriteRenderSystem>();
+    engine.AddPausableSystem<TransformSystem>();
+    engine.AddPausableSystem<AnimationSystem>();
+#if !defined(NDEBUG)
+    engine.AddSystem<DiagnosticSystem>();
+    engine.AddSystem<GUISystem>();
+    engine.AddSystem<ToolMenuSystem>();
+#endif //!defined(NDEBUG)
 }
+
+void AtonementGame::GameplaySystemsSetup()
+{
+    auto& engine = Engine::Instance();
+
+    engine.AddPausableSystem<SimpleCollisionsSystem>();
+
+    //engine.AddSystem<EditorToolSystem>();
+    //engine.AddSystem<SaveGameSystem<ECommonSaveArchetype>>(this);
+
+
+#if defined(DAGGER_DEBUG)
+#endif //defined(DAGGER_DEBUG)
+}
+
+void AtonementGame::WorldSetup()
+{
+    auto* camera = Engine::GetDefaultResource<Camera>();
+    camera->mode = ECameraMode::FixedResolution;
+    camera->size = { 1920, 1080 };
+    camera->zoom = 1;
+    camera->position = { 0, 0, 0 };
+    camera->Update();
+
+    Engine::Dispatcher().trigger<SaveGameSystem<ECommonSaveArchetype>::LoadRequest>(
+        SaveGameSystem<ECommonSaveArchetype>::LoadRequest{ "default_saved_scene.json" });
+}
+
--- source/dagger/gameplay/atonement/atonement_main.h
+++ source/dagger/gameplay/atonement/atonement_main.h
@@ -1,8 +1,23 @@
 #pragma once
 
-class atonement_main
-{
-public:
-    atonement_main();
+#include "core/core.h"
+#include "core/system.h"
+#include "core/game.h"
+#include "core/engine.h"
 
-};
+using namespace dagger;
+
+namespace atonement
+{
+    class AtonementGame : public Game
+    {
+        inline String GetIniFile() override
+        {
+            return "atonement.ini";
+        };
+
+        void CoreSystemsSetup() override;
+        void GameplaySystemsSetup() override;
+        void WorldSetup() override;
+    };
+}
