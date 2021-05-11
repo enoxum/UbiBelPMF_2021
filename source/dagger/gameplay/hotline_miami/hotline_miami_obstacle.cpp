272a30887d83d58ac38262d914f2dbcbf75386c5
Phalto <tomislavsavatijevic@gmail.com>
2021-05-11 17:01:11 +0200

    Define obstacle

---
+++ source/dagger/gameplay/hotline_miami/hotline_miami_obstacle.cpp
@@ -0,0 +1,13 @@
+#include "hotline_miami_obstacle.h"
+
+#include "core/engine.h"
+#include "core/game/transforms.h"
+#include "core/graphics/sprite.h"
+
+#include "gameplay/common/simple_collisions.h"
+
+#include <algorithm>
+#include <execution>
+
+using namespace dagger;
+using namespace hotline_miami;
---
+++ source/dagger/gameplay/hotline_miami/hotline_miami_obstacle.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "core/system.h"
+#include "core/core.h"
+
+using namespace dagger;
+
+namespace hotline_miami
+{
+    struct HotlineMiamiObstacle
+    {
+
+        bool isObstacle{ true };
+    };
+
+    class HotlineMiamiObstacleSystem
+        : public System
+    {
+    public:
+
+        inline String SystemName() { return "HotlineMiami Obstacle System"; }
+
+    };
+}
