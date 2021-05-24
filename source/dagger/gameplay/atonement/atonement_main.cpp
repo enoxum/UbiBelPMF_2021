65b7f2f8cbbd1e51fefafe52a390048daff21097
dunjx <spasicdunj3013@gmail.com>
2021-05-24 20:01:59 +0200

    Changed debug parameters for main character.

--- source/dagger/gameplay/atonement/atonement_main.cpp
+++ source/dagger/gameplay/atonement/atonement_main.cpp
@@ -152,7 +152,7 @@ void AtonementGame::WorldSetup(){
 
     SaveGameSystem<ECommonSaveArchetype>::LoadRequest{ "level_1.json" });
 
-    auto mainChar = Character::Create("ATON", { 1, 1, 1 }, { 8500, -800 }, {70, 176});
+    auto mainChar = Character::Create("ATON", { 1, 1, 1 }, { -100, -200 }, {70, 176});
     mainChar.sprite.scale = { 0.6, 0.6 };
   
     Engine::Registry().emplace<CameraFollowFocus>(mainChar.entity);
