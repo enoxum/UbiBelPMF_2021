99ec03a32c8077373232697f04ac9c06b82ae8cc
Ognjen Stamenković <ostamenkovic14@gmail.com>
2021-05-25 17:31:34 +0200

    feat: Stared work on level design
    - Added tilemap.json with design array
    - Added level cpp and h file
    - Updated mason

---
+++ source/dagger/gameplay/hotline_miami/hotline_miami_level.cpp
@@ -0,0 +1,29 @@
+#include "hotline_miami_level.h"
+
+#include "core/engine.h"
+#include "core/game/transforms.h"
+#include "core/graphics/sprite.h"
+
+#include "gameplay/common/simple_collisions.h"
+
+#include <algorithm>
+#include <execution>
+
+using namespace dagger;
+using namespace hotline_miami;
+
+void loadLevel()
+{
+
+
+
+}
+
+void loadObstacles()
+{
+
+
+
+
+
+}
---
+++ source/dagger/gameplay/hotline_miami/hotline_miami_level.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "core/system.h"
+#include "core/core.h"
+
+using namespace dagger;
+
+namespace hotline_miami
+{
+    void loadTiles();
+
+    void loadObstacles();
+}
